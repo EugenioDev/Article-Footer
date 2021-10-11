6a70c03c8638c654e21e91416eb83a51bec7f9cd
Eugenio Marino <geggiodev@Air-di-Eugenio.homenet.telecomitalia.it>
2021-10-11 14:48:18 +0200

    Validation e submit Form

--- app.js
+++ app.js
@@ -1,31 +1,61 @@
-const emailButton = document.getElementById("email-button");
-const lastIconRight = document.getElementById("more");
+window.addEventListener("DOMContentLoaded", function () {
+  const emailButton = document.getElementById("email-button");
+  const lastIconRight = document.getElementById("more");
 
-//Creare una funzione che richiama un div con all'interno un input
-//Appendere il div appena creato sotto il bottone.
-// Evento click al bottone email
-// Input Validation
+  //Creare una funzione che richiama un div con all'interno un input
+  //Appendere il div appena creato sotto il bottone.
+  // Evento click al bottone email
+  // Input Validation
 
-//Creo elemento div con interno input
-function createInput() {
-  const inputContainer = document.createElement("div");
-  inputContainer.setAttribute('class', 'inputContainer')
-  const inputField = document.createElement("input");
-  const submitButton = document.createElement('button')
+  //Creo elemento div con interno input
+  function createInput() {
+    const inputContainer = document.createElement("form");
+    inputContainer.setAttribute("class", "inputContainer");
+    inputContainer.setAttribute("novalidate", "");
+    const inputField = document.createElement("input");
+    const submitButton = document.createElement("button");
 
-  submitButton.setAttribute('type', 'submit')
-  submitButton.setAttribute('class', 'btn-form')
-  submitButton.innerHTML = '&rArr;'
-  inputContainer.appendChild(submitButton)
+    submitButton.setAttribute("class", "btn-form");
+    submitButton.innerHTML = "&rArr;";
+    inputContainer.appendChild(submitButton);
 
-  inputField.setAttribute("class", "input-field");
-  inputField.setAttribute("placeholder", "Type your Email");
-  inputField.setAttribute("type", "email");
+    inputField.setAttribute("class", "input-field");
+    inputField.setAttribute("name", "email");
+    inputField.setAttribute("placeholder", "Type your Email");
+    inputField.setAttribute("type", "email");
 
-  inputContainer.appendChild(inputField);
-  emailButton.after(inputContainer);
-}
+    inputContainer.appendChild(inputField);
+    emailButton.after(inputContainer);
 
-emailButton.onclick = function () {
-  createInput();
-};
+    //Submit Event
+    inputContainer.addEventListener("submit", checkInput);
+
+    function checkInput(e) {
+      e.preventDefault();
+      //Catturare il valore inserito nell'input
+      const inputValue = inputField.value;
+      let inputSpan = document.getElementById("emailValue");
+      const re =
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      //Eseguire la condizione che se la email è corretta modifica lo span con l'input inserito
+      if (inputValue.match(re)) {
+        inputSpan.innerHTML = inputValue;
+      } else {
+        const error = document.createElement("p");
+        error.setAttribute("class", "errormsg");
+        error.innerHTML = "Controlla i dati inseriti";
+        inputContainer.appendChild(error);
+      }
+    }
+  }
+
+  emailButton.onclick = function () {
+    if (document.querySelector("#email-button").nextElementSibling) {
+      document.querySelector("#email-button").nextElementSibling.remove();
+      document.getElementById("emailValue").innerHTML = "";
+      document.querySelector(".inputContainer").nextElementSibling.remove();
+    } else {
+      createInput();
+    }
+  };
+});
